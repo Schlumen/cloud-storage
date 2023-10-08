c2881306d244aa7c12f4e97118703746f3a0b38e
Schlumen <lumen.haendler@web.de>
2023-10-08 10:14:31 +0200

    implement react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import { AuthProvider } from "../context/AuthContext";
 import Signup from "./Signup";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <AuthProvider>
-            <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-                <div className="w-100" style={{ maxWidth: "400px" }}>
-                    <Signup />
-                </div>
-            </Container>
-        </AuthProvider>
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Router>
+                    <AuthProvider>
+                        <Routes>
+                            <Route exact path="/" Component={Dashboard} />
+                            <Route path="/signup" Component={Signup} />
+                            <Route path="/login" Component={Login} />
+                        </Routes>
+                    </AuthProvider>
+                </Router>
+            </div>
+        </Container>
 
     );
 }
