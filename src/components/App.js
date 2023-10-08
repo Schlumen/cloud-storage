7ddc11efe3fb9186e11537a19492ca34df0521fa
Schlumen <lumen.haendler@web.de>
2023-10-08 12:05:31 +0200

    Reset password

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import Signup from "./Signup";
 import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
 import Dashboard from "./Dashboard";
+import PrivateRoute from "./PrivateRoute";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../context/AuthContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                             <Route exact path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
                             <Route path="/signup" Component={Signup} />
                             <Route path="/login" Component={Login} />
+                            <Route path="/forgot-password" Component={ForgotPassword} />
                         </Routes>
                     </AuthProvider>
                 </Router>
