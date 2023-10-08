9d9a81695a66c9a1e894c9d08dbabf782cf8be99
Schlumen <lumen.haendler@web.de>
2023-10-08 17:18:38 +0200

    Add add folder button

--- src/components/drive/Dashboard.js
+++ src/components/drive/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Container } from 'react-bootstrap'
+import AddFolderButton from './AddFolderButton'
 
 export default function Dashboard() {
     return <>
         <Navbar />
-        <Container fluid>Content</Container>
+        <Container fluid>
+            <AddFolderButton></AddFolderButton>
+        </Container>
     </>
 }
