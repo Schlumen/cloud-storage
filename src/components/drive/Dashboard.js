2a326b11f8aa92956e0679859b5f13f707a24c95
Schlumen <lumen.haendler@web.de>
2023-10-11 22:11:34 +0200

    Show Child Folders

--- src/components/drive/Dashboard.js
+++ src/components/drive/Dashboard.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Container } from 'react-bootstrap'
-import AddFolderButton from './AddFolderButton'
-import { useFolder } from '../../hooks/useFolder'
+import React from 'react';
+import Navbar from './Navbar';
+import Folder from './Folder';
+import AddFolderButton from './AddFolderButton';
+import { Container } from 'react-bootstrap';
+import { useFolder } from '../../hooks/useFolder';
 
 export default function Dashboard() {
-    const { folder } = useFolder("NDJX617gS4BQERafbZ7g");
-
+    const { folder, childFolders } = useFolder("NDJX617gS4BQERafbZ7g");
+    console.log(childFolders)
     return <>
         <Navbar />
         <Container fluid>
             <AddFolderButton currentFolder={folder} />
+            {childFolders.length > 0 && (
+                <div className="d-flex flex-wrap">
+                    {childFolders.map(childFolder => {
+                        return (
+                            <div key={childFolder.id} style={{ maxWidth: "250px" }} className="p-2">
+                                <Folder folder={childFolder} />
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </Container>
     </>
 }
